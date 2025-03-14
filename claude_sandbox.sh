9aa458269f479c2375a7ce95211176d48ba46541
Mariusz Woloszyn <emsi@users.noreply.github.com>
2025-03-14 14:15:31 +0100

    fix: Update sandbox environment setup in claude_sandbox.sh

--- claude_sandbox.sh
+++ claude_sandbox.sh
@@ -7,9 +7,7 @@ SANDBOX_HOME=~/agent
 # Check if SANDBOX_HOME exists, if not create it and set ownership
 if [ ! -d "$SANDBOX_HOME" ]; then
   mkdir -p "$SANDBOX_HOME"
-  chown "$(id -u):$(id -g)" "$SANDBOX_HOME"
   cp -a ~/.bashrc "${SANDBOX_HOME}"
-
   echo 'PS1="\[\e[48;5;208m\e[97m\]sandbox\[\e[0m\] \[\e[1;32m\]\h:\w\[\e[0m\]$ "' >> "${SANDBOX_HOME}/.bashrc"
 fi
 
@@ -21,17 +19,20 @@ bwrap \
   --ro-bind /lib64 /lib64 \
   --ro-bind /etc /etc \
   --ro-bind /run/dbus /run/dbus \
-  --bind "/run/user/$(id -u)/bus" "/run/user/$(id -u)/bus" \
   --ro-bind /run/systemd /run/systemd \
   --dev-bind /dev /dev \
   --proc /proc \
+  --bind "/run/user/$(id -u)/bus" "/run/user/$(id -u)/bus" \
   --bind "${SANDBOX_HOME}" /home/agent \
-  --bind "${HOME}/.nvm/" /home/agent/.nvm/ \
+  --ro-bind "${HOME}/.nvm/" /home/agent/.nvm/ \
+  --ro-bind "${HOME}/.local/bin" /home/agent/.local/bin \
   --tmpfs /tmp \
   --clearenv \
   --setenv HOME /home/agent \
-  --setenv PATH "/usr/bin:/bin:/usr/sbin:/sbin" \
+  --setenv PATH "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/home/agent/.local/bin" \
   --setenv DISPLAY "${DISPLAY}" \
   --setenv DBUS_SESSION_BUS_ADDRESS "${DBUS_SESSION_BUS_ADDRESS}" \
+  --setenv TERM "${TERM}" \
+  --setenv COLOTTERM "${COLORTERM}" \
   /bin/bash
 
